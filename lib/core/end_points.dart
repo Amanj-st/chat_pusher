8466d5151df362391fb7e7f51d61baa71fa3cd8f
amanj <aman.stat1988@gmail.com>
2024-09-02 17:36:00 +0300

    first commit

---
+++ lib/core/end_points.dart
@@ -0,0 +1,11 @@
+class ApiEndPoints {
+  static const baseUrl = 'https://garaj.taknrx.com/api';
+
+  static const provideRoom = '/chats/provide';
+  static const rooms = '/chats/rooms';
+  static const send = '/send';
+
+ 
+
+  
+}
